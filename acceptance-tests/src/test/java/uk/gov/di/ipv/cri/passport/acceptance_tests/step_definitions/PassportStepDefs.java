76df0aea069a47019ed45132cc8ec477c5d498a4
catalin sava <catalin.sava@digital.cabinet-office.gov.uk>
2023-09-18 10:24:57 +0100

    quick fix

--- acceptance-tests/src/test/java/uk/gov/di/ipv/cri/passport/acceptance_tests/step_definitions/PassportStepDefs.java
+++ acceptance-tests/src/test/java/uk/gov/di/ipv/cri/passport/acceptance_tests/step_definitions/PassportStepDefs.java
@@ -417,7 +417,7 @@ public class PassportStepDefs extends PassportPageObject {
     }
 
     @Then("User enters expiry date as current date minus (.*) months and minus (.*) days")
-    public void expiryDateAsCurrentMinusMonths(int monthsToSubtract, int daysToSubtract) {
+    public void expiryDateMinusMonthsAndDays(int monthsToSubtract, int daysToSubtract) {
         userReEntersPassportExpiryDateMinusMonthsAndDays(monthsToSubtract, daysToSubtract);
     }
 }
