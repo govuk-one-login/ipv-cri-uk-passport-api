904fbb9af54d57bedeba566293259eeea2ad3aa0
Seamus McShane <seamus.mcshane@digital.cabinet-office.gov.uk>
2024-02-21 10:32:41 +0000

    LIME-991-smoke Fix smoke test via using the wrapper vs gradle directly.
    	- Added note to update the follow three versions together
    	- Updated cucumber_version, selenium_version and webdrivermanager_version
    	- Fixed race condition for page title assertions via waitForPageToLoad
    	- Fix setHeadless removal
    	- Added with gradle-version: wrapper to setup gradle action

--- acceptance-tests/src/test/java/uk/gov/di/ipv/cri/passport/acceptance_tests/pages/UniversalSteps.java
+++ acceptance-tests/src/test/java/uk/gov/di/ipv/cri/passport/acceptance_tests/pages/UniversalSteps.java
@@ -5,22 +5,21 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 import uk.gov.di.ipv.cri.passport.acceptance_tests.utilities.Driver;
 
-import java.time.Duration;
-
 import static org.junit.Assert.assertTrue;
+import static uk.gov.di.ipv.cri.passport.acceptance_tests.utilities.BrowserUtils.waitForPageToLoad;
 
 public class UniversalSteps {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private static final int WAIT_DELAY_SEC = 10;
+    private static final int MAX_WAIT_SEC = 10;
 
     public UniversalSteps() {
         PageFactory.initElements(Driver.get(), this);
     }
 
     public void assertPageTitle(String expTitle, boolean fuzzy) {
-        ImplicitlyWait();
+        waitForPageToLoad(MAX_WAIT_SEC);
 
         String title = Driver.get().getTitle();
 
@@ -35,15 +34,11 @@ public class UniversalSteps {
     }
 
     public void assertURLContains(String expected) {
-        ImplicitlyWait();
+        waitForPageToLoad(MAX_WAIT_SEC);
 
         String url = Driver.get().getCurrentUrl();
 
         LOGGER.info("Page url: " + url);
         assertTrue(url.contains(expected));
-    }
-
-    public static void ImplicitlyWait() {
-        Driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(WAIT_DELAY_SEC));
     }
 }
