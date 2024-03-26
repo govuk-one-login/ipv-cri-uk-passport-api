659cea726f6fcccf66912ee312bc9c68a7e37a0a
Aaron Henderson <aaron.henderson@digital.cabinet-office.gov.uk>
2024-03-26 14:29:20 +0000

    LIME-989 adding consumer version for pact testing

--- lambdas/issuecredential/src/test/java/uk/gov/di/ipv/cri/passport/issuecredential/pact/IssueCredentialHandlerTest.java
+++ lambdas/issuecredential/src/test/java/uk/gov/di/ipv/cri/passport/issuecredential/pact/IssueCredentialHandlerTest.java
@@ -7,6 +7,7 @@ import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
+import au.com.dius.pact.provider.junitsupport.loader.SelectorBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSSigner;
@@ -93,6 +94,14 @@ class IssueCredentialHandlerTest {
 
     @Mock private DataStore<SessionItem> sessionItemDataStore;
     @Mock private DataStore<PersonIdentityItem> personIdentityDataStore;
+
+    @au.com.dius.pact.provider.junitsupport.loader.PactBrokerConsumerVersionSelectors
+    public static SelectorBuilder consumerVersionSelectors() {
+        return new SelectorBuilder()
+                .tag("PassportVcProvider")
+                .branch("main", "IpvCoreBack")
+                .deployedOrReleased();
+    }
 
     @BeforeAll
     static void setupServer() {
