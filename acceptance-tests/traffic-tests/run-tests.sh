99ba40e21c9dda92661f8a516a4a3e4359d5de79
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-12-02 11:20:24 +0000

    pushd and popd added

--- acceptance-tests/traffic-tests/run-tests.sh
+++ acceptance-tests/traffic-tests/run-tests.sh
@@ -49,50 +49,50 @@ else
   export JOURNEY_TAG="${TEST_TAG}"
 fi
 
-echo "this is updated working version PARALLEL ***********"
-
-# pushd /home/gradle
-# gradle cucumber -P tags=${JOURNEY_TAG} &
-# disown
-# popd
-# sleep 10
-
-# pushd /home/gradle
-# gradle cucumber -P tags=${JOURNEY_TAG} &
-# disown
-# popd
-# sleep 10
-
-# pushd /home/gradle
-# gradle cucumber -P tags=${JOURNEY_TAG} &
-# disown
-# popd
-# sleep 10
-
-# pushd /home/gradle
-# gradle cucumber -P tags=${JOURNEY_TAG} &
-# disown
-# popd
-# sleep 10
+echo "this is updated working version PARALLEL pushd and popd ***********"
 
 pushd /home/gradle
-echo "******TEST 1 start ***********"
 gradle cucumber -P tags=${JOURNEY_TAG} &
 disown
-sleep 10
-echo "TEST 1 started ***********"
-echo "******TEST 2 START ***********"
-gradle cucumber -P tags=${JOURNEY_TAG} &
-disown
-sleep 10
-echo "TEST 2 started ***********"
-echo "******TEST 3 START ***********"
-gradle cucumber -P tags=${JOURNEY_TAG} &
-disown
-sleep 10
-echo "TEST 3 started ***********"
-echo "******TEST 4 START ***********"
-gradle cucumber -P tags=${JOURNEY_TAG} &
-disown
-wait
 popd
+sleep 10
+
+pushd /home/gradle
+gradle cucumber -P tags=${JOURNEY_TAG} &
+disown
+popd
+sleep 10
+
+pushd /home/gradle
+gradle cucumber -P tags=${JOURNEY_TAG} &
+disown
+popd
+sleep 10
+
+pushd /home/gradle
+gradle cucumber -P tags=${JOURNEY_TAG} &
+disown
+popd
+wait
+
+# pushd /home/gradle
+# echo "******TEST 1 start ***********"
+# gradle cucumber -P tags=${JOURNEY_TAG} &
+# disown
+# sleep 10
+# echo "TEST 1 started ***********"
+# echo "******TEST 2 START ***********"
+# gradle cucumber -P tags=${JOURNEY_TAG} &
+# disown
+# sleep 10
+# echo "TEST 2 started ***********"
+# echo "******TEST 3 START ***********"
+# gradle cucumber -P tags=${JOURNEY_TAG} &
+# disown
+# sleep 10
+# echo "TEST 3 started ***********"
+# echo "******TEST 4 START ***********"
+# gradle cucumber -P tags=${JOURNEY_TAG} &
+# disown
+# wait
+# popd
